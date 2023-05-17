c4ee3e938c79875c2f096d479f22e56bd904d34c
Kacper Nalepa <kacper.nlp@wp.pl>
2023-05-17 23:23:00 +0200

    Crate two routes and one default layout

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <Link href="/">Home Page</Link>
+        <Link href="/hello">Show Page</Link>
+    </header>
+    <div>Time: {{ timer }}</div>
+    <slot />
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import { ref } from "vue";
+
+const timer = ref(0);
+
+setInterval(() => timer.value++, 1000);
+</script>
---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Main Page</h1>
+    <p>{{ message }}</p>
+</template>
+
+<script setup>
+defineProps({
+    message: String,
+});
+</script>
+
+<script>
+import MainLayout from "../../Layouts/MainLayout.vue";
+
+export default {
+    layout: MainLayout,
+};
+</script>
