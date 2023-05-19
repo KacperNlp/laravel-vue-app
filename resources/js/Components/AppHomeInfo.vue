c44e9bae19787ee5d1fd29ae2bc65273fc77c2a2
Kacper Nalepa <kacper.nlp@wp.pl>
2023-05-19 15:15:30 +0200

    Style top section of card and transform price by adding dots after three digits

--- resources/js/Components/AppHomeInfo.vue
+++ resources/js/Components/AppHomeInfo.vue
@@ -1,18 +1,32 @@
 <template>
-    <div>
-        <p>City: {{ home.city }}</p>
-        <p>Beds: {{ home.beds }}</p>
-        <p>Baths: {{ home.baths }}</p>
-        <p>Area: {{ home.area }} m2</p>
-        <p>Price: ${{ home.price }}</p>
+    <div class="text-gray-500 dark:text-gray-400">
+        <div class="mb-2 text-sm">
+            <span><strong>Beds:</strong> {{ home.beds }}</span
+            ><span class="px-2">|</span
+            ><span><strong>Baths:</strong> {{ home.baths }}</span
+            ><span class="px-2">|</span
+            ><span><strong>Aerea:</strong> {{ home.area }} m²</span>
+        </div>
+        <p class="pb-2">City: {{ home.city }}</p>
+        <p>
+            Price: <strong>${{ getPrice }}</strong>
+        </p>
     </div>
 </template>
 
 <script setup>
-defineProps({
+import { computed } from "vue";
+
+const props = defineProps({
     home: {
         type: Object,
         required: true,
     },
+});
+
+const getPrice = computed(() => {
+    let price = props.home.price.toString().split(".");
+    price[0] = price[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    return price.join(",");
 });
 </script>
